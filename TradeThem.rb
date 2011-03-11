57134b36462cc89d76ff40449eb994bd97c4d4b6
Mike Cohen <mikeco@gmail.com>
2011-03-10 22:27:14 -0500

    updated TradeThem.rb

--- TradeThem.rb
+++ TradeThem.rb
@@ -6,10 +6,10 @@ twitComm = TWITTERCOMM.new
 
 #@TradeThem feed api setup
 Twitter.configure do |config|
-    config.consumer_key = "dYJIgEYjQglnvNrNoU8Bxw"
-    config.consumer_secret = "jSoAzpVVMMc38PTlE4ovEXZBNkgdXR5QQ3Nz9HgWnN4"
-    config.oauth_token = "260496289-kWGPqENvz26QZsGQRT7EpIiQTj4lNJO9dS3qhat2"
-    config.oauth_token_secret = "fGYK8sMjzzJrmmXt9wKjd3LxLwq0weUiSCUuBYWk"
+    config.consumer_key = THE_KEY
+    config.consumer_secret = THE_SECRET
+    config.oauth_token = THE_TOKEN
+    config.oauth_token_secret = THE_TOKEN_SECRET
 end
 
 puts twitComm.getMentions
