64d66cb2c1db7d9ea6a4cc1c81d4810ffcf48478
Mike Cohen <mikeco@gmail.com>
2011-03-10 22:24:36 -0500

    updated TradeThem.rb

--- TradeThem.rb
+++ TradeThem.rb
@@ -1,5 +1,8 @@
-#require 'rubygems'
+require 'rubygems'
 require 'twitter'
+require_relative "twitterComm.rb"
+
+twitComm = TWITTERCOMM.new
 
 #@TradeThem feed api setup
 Twitter.configure do |config|
@@ -9,49 +12,6 @@ Twitter.configure do |config|
     config.oauth_token_secret = "fGYK8sMjzzJrmmXt9wKjd3LxLwq0weUiSCUuBYWk"
 end
 
-#get all mentions since the first tweet mention...change when the game starts
-mentions = Twitter.mentions(:count => 50, :since_id => 45969646003822591)
-
-transactions = Array.new
-
-mentions.each do |mention|
-  mention.text.downcase!
-  theMention = mention.text
-  
-  #strip the @tradethem handle
-  theMention["@tradethem "] = ""
-  
-  #get the user the mention came from
-  from = "@" + mention.user.screen_name
-  to = ""
-  
-  #if the mention has another user's name in it then set the to var otherwise leave it
-  if theMention.include? "@" then
-    type, to, numberOfShares, company, price = mention.text.split(' ')
-  else
-    type, numberOfShares, company, price = mention.text.split(' ')
-  end
-  
-  #if BUY the buyer is the from var
-  if type=="buy" then
-    buyer = from
-    if !to.empty? then
-      seller = to
-    end
-  end
-  
-  #if SELL the buyer is the from var
-  if type=="sell" then
-    seller = from
-    if !to.empty? then
-      buyer = to
-    end
-  end 
-  
-  #add to transactions array
-  transactions.push({:type => type, :buyer => buyer, :seller => seller, :numberOfShares => numberOfShares.to_i, :company => company, :price => price.to_f, :executed => false, :transaction_date => Time.new})  
-end
-
-puts transactions
+puts twitComm.getMentions
 
  
