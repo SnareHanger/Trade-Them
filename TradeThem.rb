2b8d2a8332e307c5d087c2161807eaae3a52635d
Mike Cohen <mikeco@gmail.com>
2011-03-10 22:38:43 -0500

    changed to not give everyone my twitter api keys

--- TradeThem.rb
+++ TradeThem.rb
@@ -1,15 +1,18 @@
 require 'rubygems'
 require 'twitter'
+require 'parseconfig'
 require_relative "twitterComm.rb"
 
 twitComm = TWITTERCOMM.new
 
+pConfig = ParseConfig.new('twitter_config.txt')
+
 #@TradeThem feed api setup
 Twitter.configure do |config|
-    config.consumer_key = THE_KEY
-    config.consumer_secret = THE_SECRET
-    config.oauth_token = THE_TOKEN
-    config.oauth_token_secret = THE_TOKEN_SECRET
+    config.consumer_key = pConfig.get_value('consumer_key')
+    config.consumer_secret = pConfig.get_value('consumer_secret')
+    config.oauth_token = pConfig.get_value('oauth_token')
+    config.oauth_token_secret = pConfig.get_value('oauth_token_secret')
 end
 
 puts twitComm.getMentions
