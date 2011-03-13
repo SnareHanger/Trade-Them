ed86a8c1826d45480d64662f03db178aac29987b
Daniel <daniel@Daniels-Computer.local>
2011-03-12 21:35:03 -0500

    models and migrations - initial commit

---
+++ db/migrate/003_create_portfolio_items.rb
@@ -0,0 +1,12 @@
+class CreatePortfolioItems < ActiveRecord::Migration
+  def self.up
+    create_table :portfolio_items do |t|
+      t.integer :player_id, :null => false
+      t.integer :stock_id, :null => false
+    end
+  end
+ 
+  def self.down
+    drop_table :portfolio_items
+  end
+end
---
+++ models.rb
@@ -0,0 +1,13 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :dbfile => "tradethem.sqlite3"
+)
+
+#Player id, cash
+#PortfolioItem player_id, stock_id, purchase_price, quantity
+#Stock code price total_shares
+
+#Stock.find(:code => "foo")
+
