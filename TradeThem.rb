d191ce01662ae6d1c1342a913fa4d308e60f5a4c
Daniel <daniel@172-26-13-131.dynapool.nyu.edu>
2011-03-15 21:19:19 -0400

    whole lot of changes

--- TradeThem.rb
+++ TradeThem.rb
@@ -6,6 +6,11 @@ require 'yaml' #muahaha
 require "twitterComm"
 require 'models'
 
+def opposite_type(type)
+  return "Sell" if type =~ /buy/i
+  return "Buy" if type =~ /sell/i
+end
+
 twitComm = TWITTERCOMM.new
 
 twitter_config = YAML::load_file 'config/twitter.yml'
@@ -21,20 +26,46 @@ end
 incoming_tweets = twitComm.getMentions
 #last_tweet_id = ?
 
-twitComm.send_lack_of_shares_tweet("@SnareHanger", "Company X")
-twitComm.send_insufficient_funds("@SnareHanger")
-twitComm.send_portfolio_value_tweet("SnareHanger", 8230.25)
+#TESTING
+#twitComm.send_lack_of_shares_tweet("@SnareHanger", "Company X")
+#twitComm.send_insufficient_funds("@SnareHanger")
+#twitComm.send_portfolio_value_tweet("SnareHanger", 8230.25)
 
-
+#Go through incoming tweets and process them
 incoming_tweets.each do |tweet|
+  last_tweet_id = tweet[:id]
+
   #"buy" => "Buy" :-P
   tweet[:type].gsub!(/^(\w{1})/) {|s| s.upcase}
 
+  #see if there are any transactions pending
   txs = Transaction.active.not_executed.where(
-    :type => tweet[:type],
-    :buyer => "mike",
-    :stock => "CompA"
-  )
+    :type => opposite_type(tweet[:type]),
+    :stock => tweet[:stock]
+  ) #order - most recent first? or oldest first?  thinking oldest
+  #.order("created_at ASC")
+  #oldest_first #scope
+
+  #additional filter on buyer or seller
+  case tweet[:type]
+  when /buy/i
+    txs = txs.where(:seller => tweet[:seller])
+  when /sell/i
+    txs = txs.where(:buyer => tweet[:buyer])
+  end
+
+  #any pending transactions?
+  if txs.any?
+    tr = txs.first
+    if tr.quantity == tweet[:quantity] && tr.price == tweet[:price]
+      #tr.execute_and_tweet!
+      tr.execute!
+      #update twitter with confirmed transaction
+    else #it's a counter-offer
+      Transaction.create!(tweet) #should work, if everything was parsed correctly
+    end
+  else #no pending tweets
+  end
 end
 
 
