424ebf57ef0d3ba719cf1a95ee3d49d028988df6
Daniel <daniel@172-26-25-158.dynapool.nyu.edu>
2011-03-16 22:00:13 -0400

    added buy and sell to Player, as well as Transaction.value

--- models.rb
+++ models.rb
@@ -7,19 +7,55 @@ ActiveRecord::Base.establish_connection(
 
 class Player < ActiveRecord::Base
   has_many :portfolio_items
-  has_many :companys, :through => :portfolio_items
+  has_many :companies, :through => :portfolio_items
 
   validates_presence_of :username, :cash
   validates_numericality_of :cash
+
+  def buy!(tx)
+    self.cash -= tx.amount
+    raise InsufficentCash.new(self, tx) if self.cash < 0 #custom exception
+
+    stock = self.portfolio_items.select {|i| i.company_id == tx.company_id}.first
+
+    if stock.nil?
+      #new stock in portfolio
+      self.portfolio_items.create!(:company_id => tx.company_id, :quantity => tx.quantity)
+    else
+      #add to portfolio
+      stock.quantity += tx.quantity
+      stock.save!
+    end
+
+    self.save!
+  end
+
+  def sell!(tx)
+    #remove from portfolio
+    stock = self.portfolio_items.select {|i| i.company_id == tx.company_id}.first
+
+    #can't sell what you don't have
+    raise InsufficientStock.new(self, tx) if stock.nil?
+    raise InsufficientStock.new(self, tx) if stock.quantity < tx.quantity
+
+    stock.quantity -= tx.quantity
+    stock.save!
+
+    self.cash += tx.amount
+    self.save!
+  end
 end
 
 class Company < ActiveRecord::Base
   validates_presence_of :symbol, :price
+  validates_uniqueness_of :symbol
   validates_numericality_of :price
 end
 
+#purchase_price doesn't work for multiple purchases!!!
 class PortfolioItem < ActiveRecord::Base
-  validates_presence_of :player_id, :company_id, :purchase_price, :quantity
+  validates_presence_of :player_id, :company_id, :quantity
+  validates_uniqueness_of :company_id, :scope => :player_id
 end
 
 class Transaction < ActiveRecord::Base
@@ -33,22 +69,27 @@ class Transaction < ActiveRecord::Base
   scope :not_executed, where(:executed => false)
   scope :active, where("expiration_date < ?", Time.now)
 
-  def execute!
-    self.executed = true
-    self.save!
+  def value
+    self.price * self.quantity
+  end
 
-    #TODO: Update company price
-    self.company.update_attributes :price => self.price
-    #Update portfolio of buyer AND seller
+  def execute!
+    ActiveRecord::Base.transaction do
+      #Update portfolios of buyer AND seller
+      self.buyer.buy!(self)
+      self.seller.sell!(self)
+
+      #update company price
+      self.company.update_attributes! :price => self.price
+
+      #mark transaction as executed
+      self.update_attributes! :executed => true
+    end
   end
 end
 
 class BuyTransaction < Transaction; end
 class SellTransaction < Transaction; end
 
-#Player id, cash
-#PortfolioItem player_id, company_id, purchase_price, quantity
-#Company code price total_shares
-
-#Company.find(:code => "foo")
+#ADD Company total_shares (?)
 
