10f82c31f90bd18f8e56cc2c7265b503ec1be93e
Daniel <daniel@172-26-13-229.dynapool.nyu.edu>
2011-03-18 18:04:33 -0400

    added stuff to test

--- TradeThemTest.rb
+++ TradeThemTest.rb
@@ -1,17 +1,30 @@
 #Unit test for TradeThem
-require 'TradeThem'
+require_relative 'TradeThem'
 require 'mocha'
+
+class FakeTweet
+  attr :text
+  attr :user
+  def initialize(u, txt); self.user = User.new(u); self.text = txt; end
+end
+class User; attr :screen_name; end
+
+mentions1 = []
+mentions1 << FakeTweet.new("@daniel", "BUY please")
+mentions1 << FakeTweet.new("@mike", "SELL @daniel please")
 
 Twitter.stubs(:configure)
 
-TWITTERCOMM.any_instance.expects(:getMentions).returns(array1)
+
+Twitter.stubs(:mentions).returns(mentions1)
 TradeThem.main()
 #at this point, database should be in a certain state
 
-TWITTERCOMM.any_instance.expects(:getMentions).returns(array2)
+Twitter.stubs(:mentions).returns(mentions2)
 TradeThem.main()
 #at this point, database should be in a certain state
 
-TWITTERCOMM.any_instance.expects(:getMentions).returns(array3)
+Twitter.stubs(:mentions).returns(mentions3)
 TradeThem.main()
 #at this point, database should be in a certain state
+#Transaction.count should 2
