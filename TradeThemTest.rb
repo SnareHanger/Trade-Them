5282fa9aa5dfda4221c9b5e7da477f1ec3ffd322
Daniel <daniel@172-26-13-229.dynapool.nyu.edu>
2011-03-18 18:06:05 -0400

    added some delete_all's before each test

--- TradeThemTest.rb
+++ TradeThemTest.rb
@@ -16,15 +16,18 @@ mentions1 << FakeTweet.new("@mike", "SELL @daniel please")
 Twitter.stubs(:configure)
 
 
+Transaction.delete_all
 Twitter.stubs(:mentions).returns(mentions1)
 TradeThem.main()
 #at this point, database should be in a certain state
+#e.g. Transaction.count should 2
 
+Transaction.delete_all
 Twitter.stubs(:mentions).returns(mentions2)
 TradeThem.main()
 #at this point, database should be in a certain state
 
+Transaction.delete_all
 Twitter.stubs(:mentions).returns(mentions3)
 TradeThem.main()
 #at this point, database should be in a certain state
-#Transaction.count should 2
