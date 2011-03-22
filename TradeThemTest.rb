b8ca2ee68434a7255f756c93e7cb71bfc08625ed
Daniel <daniel@172-26-14-226.dynapool.nyu.edu>
2011-03-22 15:29:22 -0400

    beefed up tests

--- TradeThemTest.rb
+++ TradeThemTest.rb
@@ -21,6 +21,7 @@ class FakeTweet
 end
 
 class FakeMentions
+  #simple transaction
   def self.mentions1
     a = []
     a << FakeTweet.new("daniel", "@TradeThem BUY 100 COA 5.00")
@@ -28,15 +29,17 @@ class FakeMentions
     return a
   end
 
+  #price negotiation
   def self.mentions2
     a = []
     a << FakeTweet.new("daniel", "@TradeThem BUY 100 COB 5.00")
     a << FakeTweet.new("mike", "@TradeThem SELL @daniel 100 COB 5.50")
-    a << FakeTweet.new("daniel", "@TradeThem BUY 100 @mike COB 5.30")
-    a << FakeTweet.new("mike", "@TradeThem SELL @daniel COB 5.30")
+    a << FakeTweet.new("daniel", "@TradeThem BUY @mike 100 COB 5.30")
+    a << FakeTweet.new("mike", "@TradeThem SELL @daniel 100 COB 5.30")
     return a
   end
-                            
+
+  #negotiate and grab
   def self.mentions3
     a = []
     a << FakeTweet.new("daniel", "@TradeThem BUY 100 COB 5.00")
@@ -86,24 +89,95 @@ class TradeThemTest < Test::Unit::TestCase
     assert_equal 40_000, player0.cash.to_i
     assert_equal 40_000, player1.cash.to_i
 
+    assert_equal 100, player0.shares_in("COA").quantity
+    assert_equal 100, player1.shares_in("COA").quantity
+
     @tt.main
+
+    assert_equal 1, Transaction.count, "Transaction.count"
+    t = Transaction.last
+    assert t.completed?, "Transaction.last NOT completed (#{t.inspect})"
 
     player0.reload
     player1.reload
-    assert_equal 1, Transaction.count, "Transaction.count"
-    t = Transaction.first
-    assert t.completed?, "Transaction.first NOT completed (#{t.inspect})"
-    assert_equal 40_500, player1.cash.to_i
     assert_equal 39_500, player0.cash.to_i
+    assert_equal 40_500, player1.cash.to_i
+
+    assert_equal 200, player0.shares_in("COA").quantity
+    assert_equal   0, player1.shares_in("COA").quantity
   end
 
-  def Xtest2
+  def test2
     Twitter.expects(:mentions).returns(FakeMentions.mentions2)
+
+    p0 = FakeMentions.mentions2[0].user.screen_name
+    p1 = FakeMentions.mentions2[1].user.screen_name
+
+    player0 = Player.find_by_username("@"+p0)
+    player1 = Player.find_by_username("@"+p1)
+
+    assert_not_nil player0, "Could not find Player: #{p0}"
+    assert_not_nil player1, "Could not find Player: #{p1}"
+
+    assert_equal 40_000, player0.cash.to_i
+    assert_equal 40_000, player1.cash.to_i
+
+    assert_equal 100, player0.shares_in("COB").quantity
+    assert_equal 100, player1.shares_in("COB").quantity
+
     @tt.main
+
+    assert_equal 3, Transaction.count, "Transaction.count"
+    t = Transaction.last
+    assert t.completed?, "Transaction.last NOT completed (#{t.inspect})"
+
+    player0.reload
+    player1.reload
+    assert_equal 39_470, player0.cash.to_i
+    assert_equal 40_530, player1.cash.to_i
+
+    assert_equal 200, player0.shares_in("COB").quantity
+    assert_equal   0, player1.shares_in("COB").quantity
   end
 
-  def Xtest3
+  def test3
     Twitter.expects(:mentions).returns(FakeMentions.mentions3)
+
+    p0 = FakeMentions.mentions3[0].user.screen_name
+    p1 = FakeMentions.mentions3[1].user.screen_name
+    p2 = FakeMentions.mentions3[3].user.screen_name
+    assert_not_equal p0, p1
+    assert_not_equal p0, p2
+    assert_not_equal p1, p2
+
+    player0 = Player.find_by_username("@"+p0)
+    player1 = Player.find_by_username("@"+p1)
+    player2 = Player.find_by_username("@"+p2)
+
+    assert_not_nil player0, "Could not find Player: #{p0}"
+    assert_not_nil player1, "Could not find Player: #{p1}"
+    assert_not_nil player2, "Could not find Player: #{p2}"
+
+    assert_equal 40_000, player0.cash.to_i
+    assert_equal 40_000, player1.cash.to_i
+    assert_equal 40_000, player2.cash.to_i
+
+    assert_equal 100, player0.shares_in("COB").quantity
+    assert_equal 100, player1.shares_in("COB").quantity
+    assert_equal 100, player2.shares_in("COB").quantity
+
     @tt.main
+
+    assert_equal 4, Transaction.count, "Transaction.count"
+    t = Transaction.last
+    assert t.completed?, "Transaction.last NOT completed (#{t.inspect})"
+
+    player0.reload
+    player1.reload
+    player2.reload
+
+    assert_equal 39_480, player0.cash.to_i
+    assert_equal 40_000, player1.cash.to_i
+    assert_equal 40_520, player2.cash.to_i
   end
 end
