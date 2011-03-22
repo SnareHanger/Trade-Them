e5da3e6dcf38f24507d800176c022c0f83571e3d
Mike Cohen <mikeco@gmail.com>
2011-03-22 12:21:16 -0400

    fixed assets write

--- functions.rb
+++ functions.rb
@@ -17,39 +17,35 @@ end
 
 #wants a hash of company_id, current_price
 def write_stock_price_file(stock_prices)
-  CSV.open("Processing/tradethem/data/stockprices.csv","ab") do |csv|
-    csv << [stock_prices[0].price.to_s, stock_prices[1].price.to_s, stock_prices[2].price.to_s, stock_prices[3].price.to_s + "\n"]
+  CSV.open("Processing/tradethem/data/stockprices.csv","a") do |csv|
+    csv << [stock_prices[0].price.to_s, stock_prices[1].price.to_s, stock_prices[2].price.to_s, stock_prices[3].price.to_s]
   end
 end
 
-def write_assets_file(player)
-  csv_data = CSV.open("Processing/tradethem/data/assets.csv","r")
-  headers = csv_data.shift.map {|i| i.to_s }
-  
-  #creates arrays for each row of data
-  string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
-  
-  #creates a hash combining headers as keys and string_data for the values
-  array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
-  
-  playerPortfolio = PortfolioItem.find_by_player_id(player.id)
-  
-  array_of_hashes.each do |p|
-    if(p["handle"] == player.username)
-      p["COA"] = playerPortfolio.find_by_player_id_and_stock_id(player.id, "COA").quantity
-      p["COB"] = playerPortfolio.find_by_player_id_and_stock_id(player.id, "COB").quantity
-      p["COC"] = playerPortfolio.find_by_player_id_and_stock_id(player.id, "COC").quantity
-      p["COD"] = playerPortfolio.find_by_player_id_and_stock_id(player.id, "COD").quantity
-      p["CASH"] = player.cash
+def write_assets_file(players)
+  CSV.open("Processing/tradethem/data/assets.csv", "w") do |csv|
+    csv << ["handle","COA","COB","COC","COD","CASH"] 
+    cash = 0.00
+    coa, cob, coc, cod = 0
+    username = ""
+    players.each do |p|
+      puts p.inspect
+      portfolioItems = PortfolioItem.find_all_by_player_id(p.id)
+      portfolioItems.each do |pi|
+        case pi.company_id
+          when 1
+            coa = pi.quantity
+          when 2
+            cob = pi.quantity
+          when 3  
+            coc = pi.quantity
+          when 4  
+            cod = pi.quantity
+        end
+      end
+      cash = p.cash 
+      username = p.username
+      csv << [username, coa, cob, coc, cod, cash]
     end
-  end
-  
-  CSV.open("Processing/tradethem/data/assets.csv", "wb") do |csv|
-    csv << ["handle","COA","COB","COC","COD"] 
-    array_of_hashes.each do |p|
-      csv << [p["handle"], p["COA"], p["COB"], p["COC"], p["COD"], p["CASH"]]      
-    end
-  end
-  
-  
+  end 
 end
